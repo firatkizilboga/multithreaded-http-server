e8f810b59668e1636e96e58a56c823b5c5b2f63e
firatkizilboga <firatkizilboga11@gmail.com>
2024-05-14 18:49:34 +0300

    buffer implementation started

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include "blg312e.h"
 #include "request.h"
 #include <pthread.h>
+#include <sys/semaphore.h>
 #include <unistd.h>
 
 // 
@@ -23,19 +24,48 @@ void getargs(int *port, int argc, char *argv[])
     *port = atoi(argv[1]);
 }
 
+int * connfd_buff = NULL;
+int connfd_buff_top = 0;
+int num_threads = 0; // maybe a semaphore would be better
+sem_t buff_mutex;
+
+int atomic_push_to_buff(int connfd){
+    if (connfd_buff_top >= num_threads){
+        return -1;
+    }
+
+    connfd_buff[connfd_buff_top] = connfd;
+    connfd_buff_top++;
+    return 0;
+}
+
+int atomic_pop_from_buff(){
+    if (connfd_buff_top <= 0){
+        return -1;
+    }
+
+    connfd_buff_top--;
+    return connfd_buff[connfd_buff_top];
+}
 
 int main(int argc, char *argv[])
 {
     int listenfd, connfd, port, clientlen;
+    int connfd_buff_size = 0;
     struct sockaddr_in clientaddr;
 
     getargs(&port, argc, argv);
+    getargs(&num_threads, argc, argv);
+    getargs(&connfd_buff_size, argc, argv);
+    sem(&buff_mutex, 0, 1);
 
-    // 
-    // blg312e: Create some threads...
-    //
-    int * connfd_ptr;
-    
+    connfd_buff = (int *) malloc(sizeof(int) * connfd_buff_size);
+
+    if (connfd_buff == NULL){
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(1);
+    }
+
     listenfd = Open_listenfd(port);
     while (1) {
 		clientlen = sizeof(clientaddr);
@@ -51,8 +81,10 @@ int main(int argc, char *argv[])
             free(connfd_ptr);
             continue;
         }
+
         pthread_detach(tid);
     }
+    exit(1);
 }
 
 
